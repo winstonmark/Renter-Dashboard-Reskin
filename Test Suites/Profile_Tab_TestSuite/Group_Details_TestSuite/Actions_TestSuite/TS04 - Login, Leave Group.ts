66e1d735998f2fbd73bad7bc62b5b1ef373081d5
Ton <winston@poplarhomes.com>
2021-08-31 08:26:58 +0800

    [Add] Test Suite Login, Leave Group

---
+++ Test Suites/Profile_Tab_TestSuite/Group_Details_TestSuite/Actions_TestSuite/TS04 - Login, Leave Group.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS04 - Login, Leave Group</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dbb4a1d6-95d6-4115-83b6-35d3b0bd4314</testSuiteGuid>
+   <testCaseLink>
+      <guid>76247d62-e805-40b2-b3a9-87763e3650c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/login_renterdash</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d0126e1b-6ded-4f1a-8c97-8ea8acab5f43</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>47d12761-341c-424d-a2a9-8b32aba1d765</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>544de178-d5e7-49bb-9d12-0ae0535458f7</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>51447c8f-f2e3-41e1-8404-d7366db64765</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Profile_Tab/Group_Details/Actions/TC01 - Leave Group</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
