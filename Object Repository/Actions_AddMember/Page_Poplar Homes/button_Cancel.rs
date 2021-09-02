e64a60770e43c0fd60df6a417f06478bfadb2adc
Ton <winston@poplarhomes.com>
2021-09-02 09:10:33 +0800

    [Add] Cancel button in obj repo

---
+++ Object Repository/Actions_AddMember/Page_Poplar Homes/button_Cancel.rs
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Cancel</name>
+   <tag></tag>
+   <elementGuidId>5ed5fa2d-1ff7-48eb-adfa-676848cf7f44</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/main/article/div/div/div/div[2]/div[2]/div[2]/div[3]/div/div/button</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-text sm:ml-auto min-w-min text-right py-0 px-0 uppercase text-pop-orange font-semibold</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Cancel</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/main[@class=&quot;w-full flex-grow flex-shrink absolute top-20 left-0 right-0 bottom-0 lg:pl-20 xxl:pl-64 z-0&quot;]/article[@class=&quot;w-full h-full flex flex-col overflow-hidden&quot;]/div[@class=&quot;h-full overflow-y-auto&quot;]/div[@class=&quot;w-full&quot;]/div[@class=&quot;flex flex-col w-full&quot;]/div[@class=&quot;flex flex-col sm:flex-row m-4 sm:mx-6 sm:-mt-10 sm:z-10 text-white sm:space-x-6&quot;]/div[@class=&quot;absolute sm:relative sm:rounded-lg h-full w-full inset-0 z-20 bg-pop-gray-100&quot;]/div[@class=&quot;route-card flex flex-col bg-white rounded-lg py-6 px-4 sm:p-6 m-4 sm:m-0 box-border shadow-sm&quot;]/div[@class=&quot;flex flex-col w-full mt-10 space-y-10&quot;]/div[@class=&quot;sticky sm:relative flex flex-col sm:flex-row mb-auto&quot;]/div[@class=&quot;flex sm:ml-auto space-x-10 flex-grow whitespace-nowrap py-4 sm:py-0&quot;]/button[@class=&quot;btn btn-text sm:ml-auto min-w-min text-right py-0 px-0 uppercase text-pop-orange font-semibold&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/main/article/div/div/div/div[2]/div[2]/div[2]/div[3]/div/div/button</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pets'])[1]/following::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Minors'])[1]/following::button[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Upload Documents'])[2]/preceding::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Cancel']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/button</value>
+   </webElementXpaths>
+</WebElementEntity>
